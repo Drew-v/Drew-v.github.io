19266349748fe98e16f7c2b5659595c785fc7e94
Drew-v <jamesvoor@gmail.com>
2023-03-23 16:23:41 -0400

    bugfix

--- scripts.js
+++ scripts.js
@@ -1,16 +1,13 @@
 $(document).ready(function () {
     gsap.from("header", { duration: 1, y: "-100%", ease: "bounce" });
     gsap.from("section", { duration: 1, x: "-100vw", stagger: 0.5, delay: 1, ease: "power3.out" });
-// Smooth scrolling for anchor links
-$('a[href*="#"]').on('click', function (e) {
-    e.preventDefault();
 
-    $('html, body').animate(
-        {
-            scrollTop: $($(this).attr('href')).offset().top,
-        },
-        500,
-        'linear'
-    );
-});
+    $("#down-arrow a").on("click", function (e) {
+        e.preventDefault();
+        const targetSection = $($(this).attr("href"));
+        $("html, body").animate({ scrollTop: targetSection.offset().top }, 500, function () {
+            targetSection.css("opacity", 0);
+            targetSection.animate({ opacity: 1 }, 500);
+        });
+    });
 });
